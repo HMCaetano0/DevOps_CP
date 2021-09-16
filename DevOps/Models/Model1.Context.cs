1675d6cff134e8f031c5d3104c714c704a1c9419
Higor Caetano <higorcaetano2909@gmail.com>
2021-09-16 19:50:38 -0300

    first commit

---
+++ DevOps/Models/Model1.Context.cs
@@ -0,0 +1,21 @@
+﻿namespace DevOps.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    
+    public partial class UsuariosEntities : DbContext
+    {
+        public UsuariosEntities()
+            : base("name=UsuariosEntities")
+        {
+        }
+    
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
